b52a148d73a69a79982d67804350e47093b4a769
viltemeske <meshkenai@gmail.com>
2023-07-04 21:06:10 +0300

    light/dark mode change

---
+++ task_4/app.js
@@ -0,0 +1,43 @@
+let toggle_btn;
+let main_wraper;
+
+function declare() {
+    toggle_btn =  document.querySelector(".toggle-btn");
+    main_wraper = document.querySelector(".main-wraper");
+}
+
+const main =  document.querySelector("main");
+
+declare();
+
+let dark = false;
+
+function toggleAnimation() {
+    dark = !dark;
+    let clone = main_wraper.cloneNode(true);
+    if(dark){
+        clone.classList.remove("light");
+        clone.classList.add("dark");
+    } else {
+        clone.classList.remove("dark");
+        clone.classList.add("light");
+    }
+    clone.classList.add("copy");
+    main.appendChild(clone);
+
+    document.body.classList.add("stop-scrolling");
+
+    clone.addEventListener("animationend", () => {
+        document.body.classList.remove("stop-scrolling");
+        main_wraper.remove();
+        clone.classList.remove("copy");
+        declare();
+        events();
+    });
+}
+
+function events() {
+toggle_btn.addEventListener("click", toggleAnimation);
+}
+
+events();
